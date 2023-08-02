342f45ef34aeebeb4f87e2edc5dc0a2305e2cc60
adamullmann <adamullmannbusiness@gmail.com>
2023-08-01 22:06:57 -0700

    Added window scalability, set fps, improved data access, added new test images, and added a costom window icon.

--- MANHOLE REMAKE/MANHOLE REMAKE.cpp
+++ MANHOLE REMAKE/MANHOLE REMAKE.cpp
@@ -20,10 +20,11 @@ enum class Scene {
 
 // a struct to hold all scene data (assets, click boxes, etc.)
 struct ClickBox {
-    Rectangle bounds;
+    Rectangle originalBounds;
     Scene targetScene;
     Sound soundEffect;
     bool isMiniGameStart;
+    Rectangle bounds;
 };
 
 // a struct to represent a sprite
@@ -36,11 +37,12 @@ struct Sprite {
 struct SceneData {
     Texture2D image;
     std::vector<ClickBox> clickBoxes;
+    Music song;
     std::vector<Sprite> sprites;
-    // Add more scene-specific data as needed (e.g., sounds, animations)
+    // more scene-specific data as needed
 };
 
-// Function to handle scene transitions
+// a function to handle scene transitions
 void ChangeScene(Scene& currentScene, Scene nextScene) {
     currentScene = nextScene;
 }
@@ -50,40 +52,86 @@ void StartMiniGame() {
 
 }
 
+void ResizeTextures(std::map<const Scene, SceneData>& datamap, int newWidth, int newHeight) {
+    for (std::pair<const Scene, SceneData>& sceneDataPair : datamap) {      // for each scene in the map 
+        SceneData& data = sceneDataPair.second; // get the scene data from the pair
+
+        Image img = LoadImageFromTexture(data.image);   // background texture resize
+        ImageResize(&img, newWidth, newHeight);
+        UnloadTexture(data.image);
+        data.image = LoadTextureFromImage(img);
+        UnloadImage(img);
+
+                    // updates click box positions and scales based on the new window size
+        for (ClickBox& clickBox : data.clickBoxes) {
+            clickBox.bounds.x = clickBox.originalBounds.x * static_cast<float>(newWidth);
+            clickBox.bounds.y = clickBox.originalBounds.y * static_cast<float>(newHeight);
+            clickBox.bounds.width = clickBox.originalBounds.width * static_cast<float>(newWidth);
+            clickBox.bounds.height = clickBox.originalBounds.height * static_cast<float>(newHeight);
+        }
+
+                     // updates sprite positions and scales based on the new window size
+        for (auto& sprite : data.sprites) {
+            sprite.position.x *= static_cast<float>(newWidth);
+            sprite.position.y *= static_cast<float>(newHeight);
+            // add scaling code
+        }
+    }
+}
+
+
 int main(void)
 {
     // initialization
-    const int screenWidth = GetScreenWidth();
-    const int screenHeight = GetScreenHeight();
+    int screenWidth = GetScreenWidth();
+    int screenHeight = GetScreenHeight();
     InitWindow(screenWidth, screenHeight, "MANHOLE");
     ToggleFullscreen();
     InitAudioDevice();
+    SetTargetFPS(60);
     SetMouseCursor(3);
+    Image img = LoadImage("assets/ICON.png");
+    SetWindowIcon(img);
+    screenWidth = GetScreenWidth();
+    screenHeight = GetScreenHeight();
 
     Scene currentScene = Scene::MAIN_MENU;
 
-    // Load scene data into a map
-    std::map<Scene, SceneData> sceneData;
+    std::map<const Scene, SceneData> sceneData;     // all scene data is loaded into a map
 
+    // Scene 1
     sceneData[Scene::SCENE_1] = {
-        LoadTexture("assets/unknofdwn.png"),
+        LoadTexture("assets/testRenders/first.png"),
         {
-            {Rectangle{0, 0, 500, 500}, Scene::SCENE_2, LoadSound("path/to/sound_effect.wav"), false } // Clicking here leads to Scene 2
+            { 
+                Rectangle{ 0.1, 0.1, 0.2, 0.2 }, // defines click boxes
+                Scene::SCENE_2,                         //  clicking here leads to a target scene
+                LoadSound("path/to/sound_effect.wav"),  //  clicking here plays a sound
+                false                                   //  is it a minigame scene?
+            }
         }
-        // Add other assets and data for scene 1, such as sound effects, animations, etc.
+        // add other assets and data for scene 1, such as sound effects, animations, etc.
     };
 
     // Scene 2
     sceneData[Scene::SCENE_2] = {
-        LoadTexture("assets/shopping.png"),
+        LoadTexture("assets/testRenders/03.png"),
         {
-            {Rectangle{0, 0, 500, 500}, Scene::MAIN_MENU}, // Clicking here leads back to Main Menu
-            // Add other click boxes for scene 2 and their respective target scenes
+            {
+                Rectangle{ 0.1, 0.1, 0.2, 0.2 }, // defines click boxes
+                Scene::MAIN_MENU,                         //  clicking here leads to a target scene
+                LoadSound("path/to/sound_effect.wav"),  //  clicking here plays a sound
+                false                                   //  is it a minigame scene?
+            }
         }
         // Add other assets and data for scene 2, such as sound effects, animations, etc.
     };
 
     // Load data for other scenes here
+
+
+
+    ResizeTextures(sceneData, screenWidth, screenHeight);           // initial scaling
 
     // Main game loop
     while (!WindowShouldClose()) {
@@ -91,16 +139,15 @@ int main(void)
 
         // Input handling
         if (currentScene == Scene::MAIN_MENU) {
-            // Handle clicks in the main menu
+            // handle clicks in the main menu
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                // Implement logic to check if the user clicked on a button to start the game
-                // Example:
+                // implement logic to check if the user clicked on a button to start the game
                 ChangeScene(currentScene, Scene::SCENE_1);
             }
         }
         else {
 
-            // Handle clicks and interactions for other scenes
+            // handle clicks and interactions for other scenes
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
                 Vector2 mousePos = GetMousePosition();
                 SceneData& currentSceneData = sceneData[currentScene];
@@ -108,25 +155,24 @@ int main(void)
                     if (CheckCollisionPointRec(mousePos, clickBox.bounds)) {
 
                         
-                        // Perform actions related to the clicked box in the current scene
-                        // Example:
-                        // PlaySound(currentSceneData.sound);
-                        // ChangeScene(currentScene, nextScene);
-                        // Trigger animations or open minigames here
+                        // perform actions related to the clicked box in the current scene
+                        // playSound(currentSceneData.sound);
+                        // changeScene(currentScene, nextScene);
+                        // trigger animations or open minigames here
 
 
-                    // Play sound effect, if defined for this click box
+                    // play sound effect, if defined for this click box
                             PlaySound(clickBox.soundEffect);
 
-                        // Start the mini-game, if defined for this click box
+                        // start the mini-game, if defined for this click box
                         if (clickBox.isMiniGameStart) {
                             StartMiniGame(); 
                         }
 
-                        // Change scene to the target scene defined for this click box
+                        // change scene to the target scene defined for this click box
                         ChangeScene(currentScene, clickBox.targetScene);
 
-                        break; // Stop checking if the mouse is clicked in multiple click boxes
+                        break; // stop checking if the mouse is clicked in multiple click boxes
 
 
                     }
@@ -134,11 +180,20 @@ int main(void)
             }
         }
 
+        if (IsKeyPressed(KEY_SPACE)) {
+            screenWidth = 900;
+            screenHeight = 600;
+            ToggleFullscreen();
+            SetWindowSize(screenWidth, screenHeight);
+            SetWindowPosition(25, 45);
+            ResizeTextures(sceneData, screenWidth, screenHeight);
+        }
+
         // Draw
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        // Draw the appropriate scene based on the currentScene value
+        // draws appropriate scene based on the currentScene value
         if (currentScene != Scene::MAIN_MENU) {
             SceneData& currentSceneData = sceneData[currentScene];
             DrawTexture(currentSceneData.image, 0, 0, WHITE);
@@ -148,22 +203,20 @@ int main(void)
             }
         }
 
-        // Draw other UI elements here
+        // misc ui elements go here
 
         for (const ClickBox& clickBox : sceneData[currentScene].clickBoxes) { // debugging use. to highlight bounds of click boxes
-            DrawRectangleRec(clickBox.bounds, Color{ 0, 0, 200, 100 });       // debugging use. to highlight bounds of click boxes
+            DrawRectangleRec(clickBox.bounds, Color{ 250, 0, 0, 80 });       // debugging use. to highlight bounds of click boxes
         }
 
         EndDrawing();
     }
-
-    // Unload any loaded resources here
-    for (auto& scene : sceneData) {
+    for (auto& scene : sceneData) {             // unload & deallocate
         UnloadTexture(scene.second.image);
-        // Unload other scene-specific assets
+        // unload other scene-specific assets here
     }
 
-    // De-Initialization
+    // de-initialization & end
     CloseWindow();
 
     return 0;
